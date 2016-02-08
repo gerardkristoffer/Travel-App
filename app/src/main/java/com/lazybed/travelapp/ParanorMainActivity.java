d333039d6d0fd7c16fdb64bf6f03d2aa5b2d15d6
gerardkristoffer <gerardkristoffer.sabado@gmail.com>
2016-02-08 23:42:49 +0800

    first commit

---
+++ app/src/main/java/com/lazybed/travelapp/ParanorMainActivity.java
@@ -0,0 +1,13 @@
+package com.lazybed.travelapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ParanorMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paranor_main);
+    }
+}
